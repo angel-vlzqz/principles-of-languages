a0d6858982c68f0a33167bd4da9339b95908c86b
angel-vlzqz <106009767+angel-vlzqz@users.noreply.github.com>
2024-03-10 11:08:05 -0600

    add lab question 4-1 and assignment

--- projects/Logic Programming Assignment 1/main.pl
+++ projects/Logic Programming Assignment 1/main.pl
@@ -1,31 +1,101 @@
-% Define mother(X, Y) predicate
-mother(X, Y) :- parent(X, Y), female(X).
+% Define parent, male, and female predicates
+parent(john, ann).
+parent(john, bob).
+parent(sue, ann).
+parent(sue, bob).
+parent(bob, charlie).
+parent(bob, david).
 
-% Define sister(X, Y) predicate
-sister(X, Y) :- parent(Z, X), parent(Z, Y), female(X), X \= Y.
+male(john).
+male(bob).
+male(charlie).
+male(david).
 
-% Find the maximum of a list of numbers
+female(sue).
+female(ann).
+
+% Predicate to define mother(X, Y)
+mother(X, Y) :-
+    parent(X, Y),
+    female(X).
+
+% Predicate to define sister(X, Y)
+sister(X, Y) :-
+    parent(Z, X),
+    parent(Z, Y),
+    female(X),
+    X \= Y.
+
+% Predicate to find the maximum of a list of numbers
 max_list([X], X).
-max_list([H | T], Max) :- max_list(T, TailMax), (H >= TailMax, Max = H; H < TailMax, Max = TailMax).
+max_list([H|T], Max) :-
+    max_list(T, TailMax),
+    (H > TailMax -> Max = H ; Max = TailMax).
 
-% Find the intersection of two lists
+% Predicate to find the intersection of two lists
 intersection([], _, []).
-intersection([H | T], List2, [H | IntersectionTail]) :- member(H, List2), intersection(T, List2, IntersectionTail).
-intersection([H | T], List2, Intersection) :- \+ member(H, List2), intersection(T, List2, Intersection).
+intersection([H|T], List2, [H|Result]) :-
+    member(H, List2),
+    intersection(T, List2, Result).
+intersection([_|T], List2, Result) :-
+    intersection(T, List2, Result).
 
-% Find the union of two lists
+% Predicate to return the union of two lists
 union([], List, List).
-union([H | T], List, [H | UnionTail]) :- \+ member(H, List), union(T, List, UnionTail).
-union([H | T], List, Union) :- member(H, List), union(T, List, Union).
+union([H|T], List, Result) :-
+    member(H, List),
+    union(T, List, Result).
+union([H|T], List, [H|Result]) :-
+    \+ member(H, List),
+    union(T, List, Result).
 
-% Sort a list
-sort_list([], []).
-sort_list([Pivot | Rest], Sorted) :-
-partition(Rest, Pivot, Smaller, Greater),
-sort_list(Smaller, SmallerSorted),
-sort_list(Greater, GreaterSorted),
-append(SmallerSorted, [Pivot | GreaterSorted], Sorted).
-partition([], _, [], []).
-partition([H | T], Pivot, [H | Smaller], Greater) :- H < Pivot, partition(T, Pivot, Smaller, Greater).
-partition([H | T], Pivot, Smaller, [H | Greater]) :- H >= Pivot, partition(T, Pivot, Smaller, Greater).
+% Predicate to sort a list
+sort_list(List, SortedList) :-
+    predsort(compare, List, SortedList).
+
+% Test Cases
+:- begin_tests(mother).
+test(mother_1, [nondet]) :-
+    mother(sue, ann).
+test(mother_2, [fail]) :-
+    mother(john, bob).
+:- end_tests(mother).
+
+:- begin_tests(sister).
+test(sister_1, [nondet]) :-
+    sister(ann, bob).
+test(sister_2, [fail]) :-
+    sister(charlie, david).
+:- end_tests(sister).
+
+:- begin_tests(max_list).
+test(max_list_1, [nondet]) :-
+    max_list([3, 7, 1, 9, 4], 9).
+test(max_list_2, [fail]) :-
+    max_list([2, 5, 8], 2).
+:- end_tests(max_list).
+
+:- begin_tests(intersection).
+test(intersection_1, [nondet]) :-
+    intersection([1, 2, 3, 4], [3, 4, 5, 6], [3, 4]).
+test(intersection_2, [fail]) :-
+    intersection([a, b, c], [d, e, f], _).
+:- end_tests(intersection).
+
+:- begin_tests(union).
+test(union_1, [nondet]) :-
+    union([1, 2, 3], [3, 4, 5], [1, 2, 3, 4, 5]).
+test(union_2, [fail]) :-
+    union([a, b, c], [d, e, f], _).
+:- end_tests(union).
+
+:- begin_tests(sort_list).
+test(sort_list_1, [nondet]) :-
+    sort_list([3, 1, 4, 1, 5, 9, 2, 6, 5, 3, 5], [1, 1, 2, 3, 3, 4, 5, 5, 5, 6, 9]).
+test(sort_list_2, [fail]) :-
+    sort_list([a, b, c], _).
+:- end_tests(sort_list).
+
+% Run all tests
+:- run_tests.
 
--- topic questions/4-1/main.pl
+++ topic questions/4-1/main.pl
@@ -1,1 +1,35 @@
-:
+% Predicate to add an element to a list
+addElement(Element, List, Result) :-
+Result = [Element | List].
+
+% Predicate to count occurrences of a value in a list
+howMany(_, [], 0).
+howMany(Value, [Value | Tail], Count) :-
+howMany(Value, Tail, SubCount),
+Count is SubCount + 1.
+howMany(Value, [_ | Tail], Count) :-
+howMany(Value, Tail, Count).
+
+% Test cases for addElement
+:- begin_tests(addElement).
+test(addElement_1, [nondet]) :-
+addElement(2, [3, 4, 5], X),
+X = [2, 3, 4, 5].
+test(addElement_2, [nondet]) :-
+addElement(a, [b, c], X),
+X = [a, b, c].
+:- end_tests(addElement).
+
+% Test cases for howMany
+:- begin_tests(howMany).
+test(howMany_1, [nondet]) :-
+howMany(2, [3, 2, 3, 5, 2, 2], X),
+X = 4.
+test(howMany_2, [nondet]) :-
+howMany(a, [a, b, c, a, d], X),
+X = 2.
+:- end_tests(howMany).
+
+% Run all tests
+:- run_tests.
+
